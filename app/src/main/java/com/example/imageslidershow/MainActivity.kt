c87ebdb470e1616ae68567b64cf4e3ea30d05b5f
Aman Nishad <amannishad0512@gmail.com>
2023-04-19 23:12:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageslidershow/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.imageslidershow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ActionTypes
+import com.denzcoskun.imageslider.constants.AnimationTypes
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemChangeListener
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.interfaces.TouchListener
+import com.denzcoskun.imageslider.models.SlideModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider) // init imageSlider
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+        imageList.add(SlideModel(R.drawable.lord_brahma, "The Creator"))
+        imageList.add(SlideModel(R.drawable.lord_vishnu, "The Preserver"))
+        imageList.add(SlideModel(R.drawable.lord_shiva, "The Destroyer"))
+        imageList.add(SlideModel(R.drawable.karn, "Karn"))
+        imageList.add(SlideModel(R.drawable.lord_hanuman, "Mahabali BajrangBali"))
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setSlideAnimation(AnimationTypes.CUBE_IN)
+
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                // You can listen here.
+                println("normal")
+            }
+
+            override fun doubleClick(position: Int) {
+                // Do not use onItemSelected if you are using a double click listener at the same time.
+                // Its just added for specific cases.
+                // Listen for clicks under 250 milliseconds.
+                println("its double")
+            }
+        })
+
+        imageSlider.setItemChangeListener(object : ItemChangeListener {
+            override fun onItemChanged(position: Int) {
+                //println("Pos: " + position)
+            }
+        })
+
+        imageSlider.setTouchListener(object : TouchListener {
+            override fun onTouched(touched: ActionTypes, position: Int) {
+                if (touched == ActionTypes.DOWN){
+                    imageSlider.stopSliding()
+                } else if (touched == ActionTypes.UP ) {
+                    imageSlider.startSliding(1000)
+                }
+            }
+        })
+    }
+
+}
